39489d135a4f092dc7e6497037d126264bd4e6b4
TrafficConeGod <TrafficConeGod@github.com>
2022-08-25 19:04:20 -0400

    fix texture bleeding in specific case

--- shader/fragment.glsl
+++ shader/fragment.glsl
@@ -7,6 +7,6 @@ in vec2 in_uv;
 out vec4 out_color;
 
 void main() {
-    ivec2 rounded_uv = ivec2(in_uv.x, in_uv.y);
+    ivec2 rounded_uv = ivec2(clamp(in_uv.x, 48, 63), clamp(in_uv.y, 0, 15));
     out_color = texelFetch(tex_sampler, rounded_uv, 0).rgba;
 }
